d3a1c3395c6c30e20f6def9821ab3e5d480ec46b
ChrisCoolman <159795512+ChrisCoolman@users.noreply.github.com>
2024-12-17 13:41:37 -0500

    Update uzz.py
    Added a full word list

--- uzz.py
+++ uzz.py
@@ -5,11 +5,12 @@ wordList = list(word.strip())
 resultList = []
 uzz_added = False  # Flag to indicate if "uzz" has been added
 
+blockers = ['b', 'c', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'v', 'w', 'x', 'y', 'z']
+
 for i in range(len(wordList)):
     if uzz_added:
         break  # Stop processing if "uzz" has already been added
-
-    if wordList[i] == 'a' and (i + 1 >= len(wordList) or wordList[i + 1] not in ['y', 't', 'k', 'n', 'd']):
+    elif wordList[i] == 'a' and (i + 1 >= len(wordList) or wordList[i + 1] not in blockers):
         resultList.append("uzz")
         uzz_added = True  # Set the flag to True
     elif wordList[i] in ['e', 'i', 'o', 'u']:
